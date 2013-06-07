694231bb156b508aa7dae3dc600506cd50a3cbd9
razies <matthias.neumayer@hotmail.de>
2013-06-08 01:01:54 +0200

    8.6.13 final

--- JBet/src/jbet/components/Matchup.java
+++ JBet/src/jbet/components/Matchup.java
@@ -5,66 +5,67 @@ package jbet.components;
 
 /**
  * 05.06.2013
- *
+ * 
  */
 public class Matchup {
-    
+    private Matchday matchday;
     private Team team1;
     private Team team2;
     private int scoreTeam1;
     private int scoreTeam2;
+    private boolean hasScores;
 
-    public Matchup(Team team1, Team team2){
+    public Matchup(Matchday matchday, Team team1, Team team2) {
+	this.matchday = matchday;
 	this.team1 = team1;
 	this.team2 = team2;
+	this.hasScores = false;
     }
-    
-    public Matchup(Team team1, Team team2, int scoreTeam1, int scoreTeam2){
-	this(team1, team2);
+
+    public Matchup(Matchday matchday, Team team1, Team team2, int scoreTeam1,
+	    int scoreTeam2) {
+	this(matchday, team1, team2);
 	this.scoreTeam1 = scoreTeam1;
 	this.scoreTeam2 = scoreTeam2;
+	this.hasScores = true;
     }
 
     /**
      * @return the scoreTeam1
      */
     public int getScoreTeam1() {
-        return this.scoreTeam1;
+	return this.scoreTeam1;
     }
 
     /**
-     * @param scoreTeam1 the scoreTeam1 to set
+     * @param scoreTeam1
+     *            the scoreTeam1 to set
      */
-    public void setScoreTeam1(int scoreTeam1) {
-        this.scoreTeam1 = scoreTeam1;
+    public void setScores(int scoreTeam1, int scoreTeam2) {
+	this.scoreTeam1 = scoreTeam1;
+	this.scoreTeam2 = scoreTeam2;
+	this.hasScores = true;
     }
 
     /**
      * @return the scoreTeam2
      */
     public int getScoreTeam2() {
-        return this.scoreTeam2;
-    }
-
-    /**
-     * @param scoreTeam2 the scoreTeam2 to set
-     */
-    public void setScoreTeam2(int scoreTeam2) {
-        this.scoreTeam2 = scoreTeam2;
+	return this.scoreTeam2;
     }
 
     /**
      * @return the team1
      */
     public Team getTeam1() {
-        return this.team1;
+	return this.team1;
     }
 
     /**
      * @return the team2
      */
     public Team getTeam2() {
-        return this.team2;
+	return this.team2;
     }
 
     /* (non-Javadoc)
@@ -74,6 +75,9 @@ public class Matchup {
     public int hashCode() {
 	final int prime = 31;
 	int result = 1;
+	result = prime * result + (this.hasScores ? 1231 : 1237);
+	result = prime * result
+		+ ((this.matchday == null) ? 0 : this.matchday.hashCode());
 	result = prime * result + this.scoreTeam1;
 	result = prime * result + this.scoreTeam2;
 	result = prime * result
@@ -95,6 +99,13 @@ public class Matchup {
 	if (getClass() != obj.getClass())
 	    return false;
 	Matchup other = (Matchup) obj;
+	if (this.hasScores != other.hasScores)
+	    return false;
+	if (this.matchday == null) {
+	    if (other.matchday != null)
+		return false;
+	} else if (!this.matchday.equals(other.matchday))
+	    return false;
 	if (this.scoreTeam1 != other.scoreTeam1)
 	    return false;
 	if (this.scoreTeam2 != other.scoreTeam2)
@@ -112,12 +123,29 @@ public class Matchup {
 	return true;
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see java.lang.Object#toString()
      */
     @Override
     public String toString() {
-	return this.team1 + " " + this.scoreTeam1 + ":" + this.scoreTeam2 + " " + this.team2;
+	return this.matchday + "=" + this.team1 + this.scoreTeam1 + ":"
+		+ this.scoreTeam2 + this.team2;
+    }
+
+    /**
+     * @return the matchday
+     */
+    public Matchday getMatchday() {
+	return this.matchday;
+    }
+
+    /**
+     * @return the hasScores
+     */
+    public boolean hasScores() {
+        return this.hasScores;
     }
 
 }
