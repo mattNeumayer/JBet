ef6283cbadf808969f39d56bc50935fd4374c1ea
pinzeka19 <pinzeka19@termsrv2.schule.itg-dachau.tsc>
2013-06-17 10:53:28 +0200

    Fixed main

---
+++ src/jbet/Jbet.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jbet;
+
+/**
+ *
+ * @author pinzeka19
+ */
+public class Jbet {
+    public static void main(String[] args) {
+        new Controller();
+    }
+}
